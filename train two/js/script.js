b29ccb770006ab7a1e10215271456907422cfb4f
Aleksandr Panchenko <lordfreeze1986@gmail.com>
2024-01-28 18:30:22 +0700

    test 1

---
+++ train two/js/script.js
@@ -0,0 +1,35 @@
+const menuBtn = document.querySelector(".menu__btn");
+const menuMobile = document.querySelector(".header__menu-list");
+
+menuBtn.addEventListener("click", () => {
+    menuMobile.classList.toggle("menu-open");
+})
+
+const swiperOne = new Swiper('.feedback__slider', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+  const swiperTwo = new Swiper('.sertificates__slider', {
+    loop: true,
+    slidesPerView: 3 ,
+    spaceBetween: 20,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+      },
+      404: {
+        slidesPerView: 1,
+      },
+    }
+  });
+  
+  
