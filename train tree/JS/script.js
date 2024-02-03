a2394b3f1907870158b180bfbca4978449878547
Aleksander-Panchenko <157949614+Aleksander-Panchenko@users.noreply.github.com>
2024-02-03 18:11:21 +0700

    Delete train tree directory

--- train tree/JS/script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".menu__btn");
-const menuMobile = document.querySelector(".header__menu-list");
-
-menuBtn.addEventListener("click", () => {
-    menuMobile.classList.toggle("menu-open")
-})
